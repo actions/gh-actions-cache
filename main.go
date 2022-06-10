45f4742cecd019eca486b82cbcec3024b5c82edd
t-dedah <t-dedah@github.com>
2022-06-10 23:14:02 +0530

    cobra setup

--- main.go
+++ main.go
@@ -1,26 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"fmt"
-
-	"github.com/cli/go-gh"
-)
+import "github.com/t-dedah/gh-actions-cache/cmd"
 
 func main() {
-	fmt.Println("hi world, this is the gh-actions-cache extension!")
-	client, err := gh.RESTClient(nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("running as %s\n", response.Login)
+	cmd.Execute()
 }
-
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
