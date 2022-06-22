cf3b9f05b0952dd3600fa69b5219a535aaa71110
Deepak Dahiya <59823596+t-dedah@users.noreply.github.com>
2022-06-23 04:18:21 +0530

    Update model.go

--- types/model.go
+++ types/model.go
@@ -1,27 +1,27 @@
 package types
 
 type RepoLevelUsageApiResponse struct {
-	FullName				string				`json:"full_name"`
-	ActiveCacheSizeInBytes			float64				`json:"active_caches_size_in_bytes"`
-	ActiveCacheCount			float64				`json:"active_caches_count"`
+	FullName		string		`json:"full_name"`
+	ActiveCacheSizeInBytes	float64		`json:"active_caches_size_in_bytes"`
+	ActiveCacheCount	float64		`json:"active_caches_count"`
 }
 
 type ListApiResponse struct {
-	TotalCount	int            `json:"total_count"`
-	ActionsCaches	[]ActionsCache `json:"actions_caches"`
+	TotalCount		int		`json:"total_count"`
+	ActionsCaches		[]ActionsCache	`json:"actions_caches"`
 }
 
 type DeleteApiResponse struct {
-	TotalCount    int            `json:"total_count"`
-	ActionsCaches []ActionsCache `json:"actions_caches"`
+	TotalCount		int		`json:"total_count"`
+	ActionsCaches		[]ActionsCache	`json:"actions_caches"`
 }
 
 type ActionsCache struct {
-	Id             int      `json:"id"`
-	Ref            string   `json:"ref"`
-	Key            string   `json:"key"`
-	Version        string   `json:"version"`
-	LastAccessedAt string	`json:"last_accessed_at"`
-	CreatedAt      string 	`json:"created_at"`
-	SizeInBytes    float64  `json:"size_in_bytes"`
+	Id			int		`json:"id"`
+	Ref			string		`json:"ref"`
+	Key			string		`json:"key"`
+	Version			string		`json:"version"`
+	LastAccessedAt		string		`json:"last_accessed_at"`
+	CreatedAt		string		`json:"created_at"`
+	SizeInBytes		float64		`json:"size_in_bytes"`
 }
